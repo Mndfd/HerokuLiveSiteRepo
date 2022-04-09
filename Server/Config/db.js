e956c1ac953edc680f8f5f951183631047363657
Onur <the_old_republic@windowslive.com>
2022-04-09 12:27:13 -0400

    Fixed the MongoDB Atlas error

--- Server/Config/db.js
+++ Server/Config/db.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.SessionSecret = exports.HostName = exports.RemoteURI = exports.LocalURI = void 0;
 exports.LocalURI = "mongodb://127.0.0.1/contacts";
-exports.RemoteURI = "mongodb+srv://thomas:k64TLy7vaCgsKWpb@cluster0.askzb.mongodb.net/contacts?retryWrites=true&w=majority";
-exports.HostName = "localhost";
+exports.RemoteURI = "mongodb+srv://Mndfd:NvGvOYpw012Z0oCN@cluster0.hk4lh.mongodb.net/contacts?retryWrites=true&w=majority";
+exports.HostName = "MongoDB Atlas";
 exports.SessionSecret = "WEBD6201SessionSecret";
 //# sourceMappingURL=db.js.map
