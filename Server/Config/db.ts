0ea23a258740122d256ab48ac294ab2e84226aa7
Onur <the_old_republic@windowslive.com>
2022-04-09 11:56:43 -0400

    Changed it

--- Server/Config/db.ts
+++ Server/Config/db.ts
@@ -1,5 +1,5 @@
-export const LocalURI = "mongodb://127.0.0.1/contacts";
-export const RemoteURI = "mongodb+srv://thomas:k64TLy7vaCgsKWpb@cluster0.askzb.mongodb.net/contacts?retryWrites=true&w=majority"
-export const HostName = "localhost";
-//export const HostName = "MongoDB Atlas";
+export const LocalURI = "mongodb://0.0.0.0/contacts";
+export const RemoteURI = "mongodb+srv://Mndfd:NvGvOYpw012Z0oCN@cluster0.hk4lh.mongodb.net/contacts?retryWrites=true&w=majority"
+//export const HostName = "localhost";
+export const HostName = "MongoDB Atlas";
 export const SessionSecret = "WEBD6201SessionSecret";
