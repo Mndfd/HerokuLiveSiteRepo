0d143d69b8f26ccf304bbf09937e2e4eeb5747a8
Onur <the_old_republic@windowslive.com>
2022-04-09 11:56:55 -0400

    Fixed

--- Server/Config/db.ts
+++ Server/Config/db.ts
@@ -1,4 +1,4 @@
-export const LocalURI = "mongodb://0.0.0.0/contacts";
+export const LocalURI = "mongodb://127.0.0.1/contacts";
 export const RemoteURI = "mongodb+srv://Mndfd:NvGvOYpw012Z0oCN@cluster0.hk4lh.mongodb.net/contacts?retryWrites=true&w=majority"
 //export const HostName = "localhost";
 export const HostName = "MongoDB Atlas";
